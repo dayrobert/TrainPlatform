b85e2c260b668c7a24cbed747749f1a363097c44
Robert Day <a@bobday.net>
2026-03-25 12:00:03 -0400

    Initial Commit

---
+++ TrainPlatformTests/TrainPlatformTests.swift
@@ -0,0 +1,17 @@
+//
+//  TrainPlatformTests.swift
+//  TrainPlatformTests
+//
+//  Created by Bob Day on 3/25/26.
+//
+
+import Testing
+@testable import TrainPlatform
+
+struct TrainPlatformTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
